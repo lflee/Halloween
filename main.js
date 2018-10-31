f2f6bc4d3f2665c2e70d562e34e824d0ce1da98a
峰 <leelapfung@gmail.com>
2018-11-01 00:53:42 +0800

    One bar?

--- main.js
+++ main.js
@@ -1,6 +1,15 @@
 function play() {
-    //create a synth and connect it to the master output (your speakers)
+    
     var synth = new Tone.Synth().toMaster()
-    //play a middle 'C' for the duration of an 8th note
-    synth.triggerAttackRelease('C4, C5', '8n')
+    
+    synth.triggerAttackRelease('D5', '8n')
+    synth.triggerAttackRelease('G4', '8n')
+    synth.triggerAttackRelease('G4', '8n')
+    synth.triggerAttackRelease('D5', '8n')
+    synth.triggerAttackRelease('G4', '8n')
+    synth.triggerAttackRelease('G4', '8n')
+    synth.triggerAttackRelease('D5', '8n')
+    synth.triggerAttackRelease('G4', '8n')
+    synth.triggerAttackRelease('D#5', '8n')
+    synth.triggerAttackRelease('G4', '8n')
 }
